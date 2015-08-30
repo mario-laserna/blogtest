585d0473812a846bf638626b611c851ce3130b09
mario <laserna.mario@gmail.com>
2015-08-30 17:14:21 -0500

    inclusion de articulos

--- wp-content/themes/mario/index.php
+++ wp-content/themes/mario/index.php
@@ -12,17 +12,25 @@
 	<h2><?php bloginfo('description'); ?></h2>
 </header>
 
-<ul>
-	<li><?php bloginfo('name'); ?></li>
-	<li><?php bloginfo('description'); ?></li>
-	<li><?php bloginfo('wpurl'); ?></li>
-	<li><?php bloginfo('admin_email'); ?></li>
-	<li><?php bloginfo('charset'); ?></li>
-	<li><?php bloginfo('version'); ?></li>
-	<li><?php bloginfo('html_type'); ?></li>
-	<li><?php bloginfo('language'); ?></li>
-	<li><?php bloginfo('stylesheet_directory'); ?></li>
-	<li><?php bloginfo('stylesheet_url'); ?></li>
-</ul>
+<section class="content">
+	<article>
+		<header>
+			<h3>Titulo</h3>
+		</header>
+		<p>Texto del articulo</p>
+	</article>
+	<article>
+		<header>
+			<h3>Titulo</h3>
+		</header>
+		<p>Texto del articulo</p>
+	</article>
+	<article>
+		<header>
+			<h3>Titulo</h3>
+		</header>
+		<p>Texto del articulo</p>
+	</article>
+</section>
 </body>
 </html>
