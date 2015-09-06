445f8a30b05f0b21963bd840477fe1e21cb61499
mario <laserna.mario@gmail.com>
2015-09-05 20:46:58 -0500

    se crea template para categoria especifica a partir de su id

---
+++ wp-content/themes/mario/category.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+<body>
+
+<header class="header">
+	<h1><?php bloginfo('name'); ?></h1>
+	<h2><?php bloginfo('description'); ?></h2>
+</header>
+
+<section class="content">
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+		<article class="container__item">
+			<header>
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				<div class="datos">
+					<strong><?php the_author(); ?></strong> -
+					<small><?php the_date(); ?></small>
+				</div>
+			</header>
+			<figure>
+				<?php the_post_thumbnail('my-size'); ?>
+			</figure>
+			<?php the_excerpt();  ?>
+			<?php the_category(); ?>
+		</article>
+		
+
+	<?php endwhile; ?>
+
+	<?php else: ?>	
+
+	<?php endif; ?>
+
+</section>
+
+<?php get_footer('negro'); ?>
--- wp-content/themes/mario/page.php
+++ wp-content/themes/mario/page.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 <body>
 
+<section class="content">
 	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+	<article class="container__item">
 		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-
+	</article>
 	<?php endwhile; ?>
 
 	<?php else: ?>	
 
 	<?php endif; ?>
 
-
+</section>
 
 <?php get_footer(); ?>
